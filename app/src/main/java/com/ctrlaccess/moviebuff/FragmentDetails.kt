03edb6d33b380addc8beac4244af3a922a6e5b65
galemu00 <galemu00@galemu00-Nitro-AN515-51>
2021-09-02 14:21:45 -0400

    Start implementing main and details fragment.  Include NavGraph.

---
+++ app/src/main/java/com/ctrlaccess/moviebuff/FragmentDetails.kt
@@ -0,0 +1,6 @@
+package com.ctrlaccess.moviebuff
+
+import androidx.fragment.app.Fragment
+
+class FragmentDetails : Fragment(R.layout.fragment_details) {
+}
---
+++ app/src/main/java/com/ctrlaccess/moviebuff/FragmentMain.kt
@@ -0,0 +1,6 @@
+package com.ctrlaccess.moviebuff
+
+import androidx.fragment.app.Fragment
+
+class FragmentMain :Fragment(R.layout.fragment_main){
+}

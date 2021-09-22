8a9505b687d20996ca1e614830f4f2493ccda4b6
galemu00 <galemu00@galemu00-Nitro-AN515-51>
2021-09-22 11:56:44 -0400

    Populate details fragment when navigating from current movies.

--- app/src/main/java/com/ctrlaccess/moviebuff/ui/fragment2/FragmentDetails.kt
+++ app/src/main/java/com/ctrlaccess/moviebuff/ui/fragment2/FragmentDetails.kt
@@ -1,9 +1,39 @@
 package com.ctrlaccess.moviebuff.ui.fragment2
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.ctrlaccess.moviebuff.R
 import dagger.hilt.android.AndroidEntryPoint
+import com.ctrlaccess.moviebuff.databinding.FragmentDetailsBinding
+import com.ctrlaccess.moviebuff.util.UtilObjects.getImageUrl
 
 @AndroidEntryPoint
 class FragmentDetails : Fragment(R.layout.fragment_details) {
+
+    private val args by navArgs<FragmentDetailsArgs>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentDetailsBinding.bind(view)
+
+        binding.apply {
+            val poster = getImageUrl(args.movie)
+            Glide.with(imageViewMovieDetailPoster)
+                .load(poster)
+                .error(R.drawable.movie_place_holder)
+                .centerCrop()
+                .into(imageViewMovieDetailPoster)
+
+            val title = args.movie.title ?: " -- "
+            val releaseDate = args.movie.release_date ?: " -- "
+            val overview = args.movie.overview ?: " -- "
+
+            textViewMovieDetailsTitle.text = title
+            textViewMovieDetailsReleaseDate.text = releaseDate
+            textViewMovieDetailsOverview.text = overview
+        }
+    }
 }

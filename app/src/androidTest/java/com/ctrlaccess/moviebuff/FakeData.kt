833d1603bd330d54a68982b05fea761146892505
galemu00 <galemu00@galemu00-Nitro-AN515-51>
2021-09-27 20:37:54 -0400

    minor refactor.

---
+++ app/src/androidTest/java/com/ctrlaccess/moviebuff/FakeData.kt
@@ -0,0 +1,17 @@
+package com.ctrlaccess.moviebuff
+
+import com.ctrlaccess.moviebuff.data.model.Result
+
+object FakeData {
+
+    val movies = Result(
+        genre_ids = listOf(1, 2),
+        id = 1,
+        overview = "overview",
+        poster_path = "poster_path",
+        release_date = "yesterday",
+        title = "best movie",
+        vote_average = 2.0f
+    )
+
+}
--- app/src/androidTest/java/com/ctrlaccess/moviebuff/ui/fragments/FragmentDetailsTest.kt
+++ app/src/androidTest/java/com/ctrlaccess/moviebuff/ui/fragments/FragmentDetailsTest.kt
@@ -4,6 +4,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.MediumTest
+import com.ctrlaccess.moviebuff.FakeData
 import com.ctrlaccess.moviebuff.R
 import com.ctrlaccess.moviebuff.data.model.Result
 import com.ctrlaccess.moviebuff.launchFragmentInHiltContainer
@@ -39,20 +40,12 @@ class FragmentDetailsTest {
 
     @Test
     fun activateFragmentDetails_DisplayUi() {
-        val movies = Result(
-            genre_ids = listOf(1, 2),
-            id = 1,
-            overview = "overview",
-            poster_path = "poster_path",
-            release_date = "yesterday",
-            title = "best movie",
-            vote_average = 2.0f
-        )
+        val movies = FakeData.movies
         val bundle = FragmentDetailsArgs(movies).toBundle()
         launchFragmentInHiltContainer<FragmentDetails>(
             fragmentArgs = bundle,
             themeResId = R.style.Theme_MovieBuff
-        ) { }
+        )
 
         onView(withId(R.id.textView_movie_details_release_date)).check(matches(isDisplayed()))
         onView(withId(R.id.textView_movie_details_release_date)).check(matches(withText("yesterday")))
